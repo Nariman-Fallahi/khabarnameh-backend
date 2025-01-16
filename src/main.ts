479581020e84271a2aaa19ee36110f9f97902574
Nariman-Fallahi <programming.nf@gmail.com>
2025-01-16 06:23:47 -0600

    Add Swagger support for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('KhabarNameh')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   app.enableCors({
     origin: ['*'],
     allowedHeaders: 'Content-Type,Authorization',
